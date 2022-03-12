ae299e02aa1d90b442fef168ff334e00b5eb047a
Mateo Sam <sammateo4@gmail.com>
2022-03-12 09:11:51 -0400

    unsure

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,29 @@
-public class Welcome 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Welcome
 {
+    protected JLabel welcomeMessage;
+    protected JTextField zooKeeperName;
+    protected JButton continueButton;
+    protected JPanel panel;
     public Welcome() 
     {
+        welcomeMessage = new JLabel("Welcome to the Zoo Manager System");
+        zooKeeperName = new JTextField();
+        continueButton = new JButton("Continue");
+        panel = new JPanel();
+        
 
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     }//Welcome constructor
 
     public void displayWelcome()
     {
         
+        panel.add(welcomeMessage);
+        panel.add(zooKeeperName);
+        panel.add(continueButton);
+
     }//displayWelcome
 }//Welcome

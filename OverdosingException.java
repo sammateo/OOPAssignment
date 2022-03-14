fba0ebe9357ef136506ac8d5f2dd8f5b8bd5477c
Mateo Sam <sammateo4@gmail.com>
2022-03-14 10:36:52 -0400

    Feeding list to console

--- OverdosingException.java
+++ OverdosingException.java
@@ -1,3 +1,11 @@
-public class OverdosingException {
-    
+public class OverdosingException extends Exception
+{
+    public OverdosingException()
+    {
+        super();
+    }// OverdosingException
+
+    public String toString(){
+        return("Animal overdosed and died");
+    }//toString
 }//class OverdosingException
